4d3fd155358d63ac53393b26028b3ca916a2894d
ImadEddine-Aouinti <imadeddineaouinti010@gmail.com>
2025-07-04 10:13:44 +0100

    Initial commit

---
+++ src/main/java/ump/comparateur/Main.java
@@ -0,0 +1,8 @@
+package ump.comparateur;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Lancement du comparateur de fichiers .properties");
+    }
+}
+
