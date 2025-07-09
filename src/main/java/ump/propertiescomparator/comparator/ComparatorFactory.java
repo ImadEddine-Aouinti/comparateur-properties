ba8fbc263b8e2d8f1b14d2b875444c37b6676be7
ImadEddine-Aouinti <imadeddineaouinti010@gmail.com>
2025-07-09 11:30:01 +0100

    la classe principale de la comparison

--- src/main/java/ump/propertiescomparator/comparator/ComparatorFactory.java
+++ src/main/java/ump/propertiescomparator/comparator/ComparatorFactory.java
@@ -1,4 +1,21 @@
 package ump.propertiescomparator.comparator;
 
+import ump.propertiescomparator.comparator.impl.AdvancedDiff;
+import ump.propertiescomparator.comparator.impl.FuzzyDiff;
+import ump.propertiescomparator.comparator.impl.SimpleDiff;
+import ump.propertiescomparator.util.Logger;
+
 public class ComparatorFactory {
+    public PropertiesComparator getComparator(String type){
+        switch (type.toLowerCase()){
+            case "simple" :
+                return new SimpleDiff();
+            case "avance" :
+                return new AdvancedDiff();
+            case "Fuzzy" :
+                return new FuzzyDiff();
+            default:
+                Logger.erreur("verifier le type de la comparison ");
+        }
+    }
 }
--- src/main/java/ump/propertiescomparator/model/ComparisonResult.java
+++ src/main/java/ump/propertiescomparator/model/ComparisonResult.java
@@ -1,4 +1,23 @@
 package ump.propertiescomparator.model;
 
+import java.util.Map;
+
 public class ComparisonResult {
+    private final String file1;
+    private final String file2;
+    private final Map<String,String> diff ;
+    public ComparisonResult(String file1,String file2,Map<String,String> diff){
+        this.file1=file1;
+        this.file2=file2;
+        this.diff=diff;
+    }
+    public String getFile1(){
+        return file1;
+    }
+    public String getFile2(){
+        return file2;
+    }
+    public Map<String,String> getDiff(){
+        return diff;
+    }
 }
